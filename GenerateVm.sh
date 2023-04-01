cfb78569aa5d3a577b3f97f9fd4b59d1b9363172
Benjamin Stoll <bstoll291997@gmail.com>
2023-03-31 20:46:21 -0500

    Finished variable collection in shell script

--- GenerateVm.sh
+++ GenerateVm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 working_dir=`pwd`
-image_dir="${working_dir}VmImages/"
+image_dir="${working_dir}/VmImages/"
 
+
+# Collect variables to be used for VM creation
 # VM Name
 printf "\nPlease enter a VM name: "
 read vm_name
@@ -43,4 +45,42 @@ fi
 if ! [ -f ${iso_name} ]; then
     echo "Could not find an image named ${iso_name}"
     echo "Please make sure it exists in ${image_dir}"
+    exit 1
 fi
+
+# VM Storage Capacity
+printf "Please enter desired VM storage capacity (in MB): "
+read stg_count
+
+if [ -z ${stg_count} ]; then
+    echo "Please provide a stg count."
+    exit 1
+fi
+
+# VM Storage Mount
+printf "Please enter path to volume location: "
+read volume_path
+
+if [ -z ${volume_path} ]; then
+    echo "Please provide a path where the storage for the vm is to be kept."
+    exit 1
+fi
+
+# VM Storage Type
+printf "Please enter desired VM storage type (fixed or default): "
+read stg_type
+
+if [ -z ${stg_type} ]; then
+    echo "Please provide a stg type."
+    exit 1
+fi
+
+printf "\nCreating VM with the following parameters...\n\n"
+echo "VM Name: ${vm_name}"
+echo "CPU: ${cpu_count}"
+echo "Memory: ${mem_count}"
+echo "ISO: ${iso_name}"
+echo "Storage Capacity: ${stg_count} MB"
+echo "Storage Volume Location: ${volume_path}"
+echo "Storage Type: ${stg_type}"
+
